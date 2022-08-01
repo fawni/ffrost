af660d291c8f26738235f528134444b81447893e
obscurity <z@x4.pm>
2022-08-01 03:47:57 +0200

    refactor: clean up code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use std::env;
-use std::fs;
-use std::process::Command;
+use std::{env, fs, process::Command};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path = if args.len() > 1 { &args[1] } else { "." };
-    let files = fs::read_dir(path).unwrap();
+    let files = fs::read_dir(path).expect("could not read files in the current directory");
 
-    let total_duration = calculate_total(files);
+    let total_duration = calculate_total(files).unwrap();
     match total_duration {
         0 => println!("no accepted media files found"),
         _ => println!(
@@ -19,59 +17,35 @@ fn main() {
     }
 }
 
-fn calculate_total(paths: std::fs::ReadDir) -> u32 {
+fn calculate_total(directories: std::fs::ReadDir) -> Option<u32> {
     let accepted_extensions = ["mkv", "mp4", "webm", "mp3", "flac", "wav", "m4a"];
     let mut total_duration: f64 = 0.0;
 
-    for path in paths {
-        // dont count if:
-        // its a folder
-        if path.as_ref().unwrap().path().is_dir()
-            // or if its a dotfile
-            || path
-                .as_ref()
-                .unwrap()
-                .path()
-                .file_stem()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .starts_with(".")
-            // or if it ends with an extension that isnt accepted
-            || !accepted_extensions.contains(
-                &path
-                    .as_ref()
-                    .unwrap()
-                    .path()
-                    .extension()
-                    .unwrap()
-                    .to_str()
-                    .unwrap(),
-            )
-        {
-            continue;
-        };
+    for directory in directories {
+        let path = directory.ok()?.path();
 
-        let output = Command::new("ffprobe")
-            .arg(path.unwrap().path())
-            .arg("-v")
-            .arg("quiet")
-            .arg("-of")
-            .arg("default=nw=1:nk=1")
-            .arg("-show_entries")
-            .arg("format=duration")
-            .output()
-            .expect("process failed to execute")
-            .stdout;
+        if path.is_file() && accepted_extensions.contains(&path.extension()?.to_str()?) {
+            let output = Command::new("ffprobe")
+                .arg(path)
+                .arg("-v")
+                .arg("quiet")
+                .arg("-of")
+                .arg("default=nw=1:nk=1")
+                .arg("-show_entries")
+                .arg("format=duration")
+                .output()
+                .expect("process failed to execute")
+                .stdout;
 
-        let duration: f64 = String::from_utf8(output)
-            .unwrap()
-            .trim_end_matches("\r\n")
-            .parse::<f64>()
-            .unwrap();
+            let duration: f64 = String::from_utf8(output)
+                .ok()?
+                .trim_end_matches("\r\n")
+                .parse::<f64>()
+                .ok()?;
 
-        total_duration += duration;
+            total_duration += duration;
+        }
     }
 
-    total_duration as u32
+    Some(total_duration as u32)
 }
