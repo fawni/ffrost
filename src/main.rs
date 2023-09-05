71677f4ccbcaeb84d441d0acc2a265ebaa746931
fawn <fawn@envs.net>
2023-09-05 17:27:31 +0300

    rewrite because it's funny

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,63 @@
-use std::{env, fs, process::Command};
+use std::{ffi::OsStr, process::Command};
 
-fn main() {
-    let path = env::args().nth(1).unwrap_or_else(|| ".".to_owned());
-    let files = fs::read_dir(path).expect("could not read files in the current directory");
+type Error = Box<dyn std::error::Error>;
+type Result<T, E = Error> = std::result::Result<T, E>;
 
-    let total_duration = calculate_total(files);
-    match total_duration {
-        None => println!("no accepted media files found"),
-        Some(duration) => println!(
-            "{:02}:{:02}:{:02}",
-            ((duration / 60) / 60),
-            ((duration / 60) % 60),
-            (duration % 60)
-        ),
-    }
+fn main() -> Result<()> {
+    let path = std::env::args().nth(1).unwrap_or(".".to_owned());
+
+    match calculate(std::fs::read_dir(&path)?) {
+        Ok(duration) => {
+            println!(
+                "\x1b[1mPath: \x1b[36m{}\x1b[0;1m\nTotal Duration: \x1b[35m{:02}:{:02}:{:02}\x1b[0m",
+                std::fs::canonicalize(path)?.to_string_lossy().trim_start_matches("\\\\?\\"),
+                ((duration / 60) / 60),
+                ((duration / 60) % 60),
+                (duration % 60)
+            );
+        }
+        Err(e) => println!("\x1b[1;31merror\x1b[0;1m: {e}"),
+    };
+
+    Ok(())
 }
 
-fn calculate_total(directories: std::fs::ReadDir) -> Option<u32> {
-    let accepted_extensions = ["mkv", "mp4", "webm", "mp3", "flac", "wav", "m4a"];
-    let mut total_duration: f64 = 0.0;
+fn calculate(directories: std::fs::ReadDir) -> Result<u32> {
+    let extensions = ["mkv", "mp4", "webm", "mp3", "flac", "wav", "m4a"];
+    let mut total = 0.0;
 
     for directory in directories {
-        let path = directory.ok()?.path();
+        let path = directory?.path();
+        let ext = path.extension().and_then(OsStr::to_str).unwrap_or("");
 
-        if path.is_file() && accepted_extensions.contains(&path.extension()?.to_str()?) {
+        if path.is_file() && extensions.contains(&ext) {
             let output = Command::new("ffprobe")
                 .arg(path)
-                .arg("-v")
-                .arg("quiet")
-                .arg("-of")
-                .arg("default=nw=1:nk=1")
-                .arg("-show_entries")
-                .arg("format=duration")
-                .output()
-                .expect("process failed to execute")
-                .stdout;
+                .args([
+                    "-v",
+                    "quiet",
+                    "-of",
+                    "default=nw=1:nk=1",
+                    "-show_entries",
+                    "format=duration",
+                ])
+                .output()?;
 
-            let duration: f64 = String::from_utf8(output)
-                .ok()?
+            let duration = String::from_utf8(output.stdout)?
                 .trim_end_matches("\r\n")
-                .parse::<f64>()
-                .ok()?;
+                .parse::<f64>()?;
 
-            total_duration += duration;
+            total += duration;
+        } else if path.is_dir() {
+            if let Ok(duration) = calculate(std::fs::read_dir(path)?) {
+                total += f64::from(duration);
+            }
         }
     }
 
-    match total_duration as u32 {
-        0 => None,
-        _ => Some(total_duration as u32),
+    if total == 0.0 {
+        return Err("no accepted media files found".into());
     }
+
+    Ok(total as u32)
 }
