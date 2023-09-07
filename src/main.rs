e65460bbe533a0b99f50a177f2085bfd6534d790
fawn <fawn@envs.net>
2023-09-07 11:20:08 +0300

    erm

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
-use std::{ffi::OsStr, process::Command};
+use std::{env, ffi::OsStr, fs, process::Command};
 
 type Error = Box<dyn std::error::Error>;
 type Result<T, E = Error> = std::result::Result<T, E>;
 
 fn main() -> Result<()> {
-    let path = std::env::args().nth(1).unwrap_or(".".to_owned());
+    let path = env::args().nth(1).unwrap_or(".".to_owned());
 
-    match calculate(std::fs::read_dir(&path)?) {
+    match calculate(fs::read_dir(&path)?) {
         Ok(duration) => {
             println!(
                 "\x1b[1mPath: \x1b[36m{}\x1b[0;1m\nTotal Duration: \x1b[35m{:02}:{:02}:{:02}\x1b[0m",
-                std::fs::canonicalize(path)?.to_string_lossy().trim_start_matches("\\\\?\\"),
+                fs::canonicalize(path)?.to_string_lossy().trim_start_matches("\\\\?\\"),
                 ((duration / 60) / 60),
                 ((duration / 60) % 60),
                 (duration % 60)
             );
         }
-        Err(e) => println!("\x1b[1;31merror\x1b[0;1m: {e}"),
+        Err(e) => println!("\x1b[1;31merror\x1b[0m: \x1b[1m{e}\x1b[0m"),
     };
 
     Ok(())
 }
 
-fn calculate(directories: std::fs::ReadDir) -> Result<u32> {
+fn calculate(directories: fs::ReadDir) -> Result<u32> {
     let extensions = ["mkv", "mp4", "webm", "mp3", "flac", "wav", "m4a"];
     let mut total = 0.0;
 
@@ -49,14 +49,14 @@ fn calculate(directories: std::fs::ReadDir) -> Result<u32> {
 
             total += duration;
         } else if path.is_dir() {
-            if let Ok(duration) = calculate(std::fs::read_dir(path)?) {
+            if let Ok(duration) = calculate(fs::read_dir(path)?) {
                 total += f64::from(duration);
             }
         }
     }
 
     if total == 0.0 {
-        return Err("no accepted media files found".into());
+        return Err("No media files found".into());
     }
 
     Ok(total as u32)
